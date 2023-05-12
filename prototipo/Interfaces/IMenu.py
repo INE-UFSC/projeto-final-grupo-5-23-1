0e3ffe635fafb17ec8f865ab3122de750116efcb
Vitor Calegari <vitorprca@gmail.com>
2023-05-12 11:29:01 -0300

    Cria interface para um menu genérico

--- prototipo/Interfaces/IMenu.py
+++ prototipo/Interfaces/IMenu.py
@@ -3,10 +3,16 @@ from abc import ABC, abstractmethod
 
 class IMenu(ABC):
 
+    #[TODO] - Implementar contrutor de janela/tela
+
     @abstractmethod
-    def abrir():
+    def __init__(self):
+        self.__janela
+
+    @abstractmethod
+    def abrir(self):
         pass
 
     @abstractmethod
-    def fechar():
+    def fechar(self):
         pass
