b851680c4e6130d193c0fef1ee63dbb425e12270
Vitor Calegari <vitorprca@gmail.com>
2023-05-12 11:27:35 -0300

    Adiciona imports e melhora tipagem

--- prototipo/Interfaces/IControladorMapas.py
+++ prototipo/Interfaces/IControladorMapas.py
@@ -1,16 +1,24 @@
 from abc import ABC, abstractmethod
 
+from pygame import Surface
+
+from Interfaces.IMapa import IMapa
+
 
 class IControladorMapas(ABC):
 
     @abstractmethod
     def __init__(self):
         self.__mapas = {}
+
+    @property
+    def mapas(self):
+        return self.__mapas
     
     @abstractmethod
-    def inclui_mapa(self, mapa):
+    def inclui_mapa(self, mapa: IMapa):
         pass
 
     @abstractmethod
-    def desenha_mapa(self, id_mapa: str):
+    def desenha_mapa(self, id_mapa: str, tela: Surface):
         pass
