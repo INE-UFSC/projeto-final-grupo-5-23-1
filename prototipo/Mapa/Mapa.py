ce024297d069063d699e5e94beddffd3bd9a6046
Vitor Calegari <vitorprca@gmail.com>
2023-05-12 11:31:03 -0300

    Adiciona herança entre a classe Mapa e IMapa

--- prototipo/Mapa/Mapa.py
+++ prototipo/Mapa/Mapa.py
@@ -1,14 +1,13 @@
-import pygame
-import sys
+from pygame import Surface
+from Mapa.Bloco import Bloco
+from Interfaces.IMapa import IMapa
 
-from Bloco import Bloco
-
-class Mapa:
+class Mapa(IMapa):
     def __init__(self):
+        self.__id = 'campo'
         self.__blocos = []
         self.construir_blocos()
 
-#Property===============================
     @property
     def blocos(self):
         return self.__blocos
@@ -17,19 +16,22 @@ class Mapa:
     def blocos(self, blocos):
         self.__blocos = blocos
         return
-#=======================================
+    
+    @property
+    def id(self):
+        return self.__id
 
     def construir_blocos(self):
         largura_bloco = 60
         altura_bloco = 60
-        qtd_blocos_x = 1280 // largura_bloco #self.jogo.largura // largura_bloco
-        qtd_blocos_y = 720 // altura_bloco #self.jogo.altura // altura_bloco
+        qtd_blocos_x = 1280 // largura_bloco
+        qtd_blocos_y = 720 // altura_bloco
         
         for x in range(qtd_blocos_x):
             for y in range(qtd_blocos_y):
                 bloco = Bloco(x * largura_bloco, y * altura_bloco, largura_bloco, altura_bloco)
                 self.blocos.append(bloco)
 
-    def desenhar(self, tela):
+    def desenhar(self, tela: Surface):
         for bloco in self.blocos:
             bloco.desenhar(tela)
