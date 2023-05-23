5f95c8ecc8fac754813a4989712a927aec3499ec
Vitor Calegari <vitorprca@gmail.com>
2023-05-22 23:37:50 -0300

    Modifica a interação para uma forma mais segura e genérica

--- novo_prototipo/Mapa/Blocos/Grama.py
+++ novo_prototipo/Mapa/Blocos/Grama.py
@@ -1,13 +1,14 @@
 import pygame
 from Mapa.Blocos.BlocoComInteracao import BlocoComInteracao
 from Mapa.Blocos.TerraArada import TerraArada
+from itens.ferramentas.Enxada import Enxada
 
 class BlocoDeGrama(BlocoComInteracao):
     def __init__(self, x, y, largura, altura, observador):
         super().__init__(x, y, largura, altura, observador)
     
     def interagir(self, item):
-        if item == 'Enxada':
+        if isinstance(item, Enxada):
             self.notifica_troca_bloco(self.posicao_matriz[0], self.posicao_matriz[1], TerraArada(self.x, self.y, self.largura, self.altura, self.observador))
         
     def desenhar(self, tela):
