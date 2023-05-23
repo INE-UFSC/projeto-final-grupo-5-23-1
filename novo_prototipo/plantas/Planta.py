58354fadadbc7e81b65731834be04ba5b77d56f6
Vitor Calegari <vitorprca@gmail.com>
2023-05-22 23:39:59 -0300

    Adiciona classe planta (Precisa ser refatorada posteriormente)

---
+++ novo_prototipo/plantas/Planta.py
@@ -0,0 +1,36 @@
+import pygame
+
+class Planta(pygame.sprite.Sprite):
+
+    def __init__(self, nome, pos, group):
+        super().__init__(group)
+        self.__nome = nome
+        self.__image = pygame.Surface((10,10))
+        self.__image.fill('saddlebrown')
+        self.__rect = self.__image.get_rect(midbottom = pos)
+        self.__taxa_de_crescimento = 10
+        self.__progresso_crescimento = 0
+        self.__em_crescimento = True
+
+    @property
+    def image(self):
+        return self.__image
+    
+    @property
+    def rect(self):
+        return self.__rect
+    
+    def update(self):
+        if self.__em_crescimento:
+            self.__progresso_crescimento +=  self.__taxa_de_crescimento
+            posicao_antiga = self.__rect.midbottom
+            if self.__progresso_crescimento < 1000:
+                self.__image = pygame.Surface((10,10))
+                self.__image.fill('saddlebrown')
+            elif self.__progresso_crescimento < 2000: 
+                self.__image = pygame.Surface((20,40))
+                self.__image.fill('chartreuse3')
+            elif self.__progresso_crescimento < 4000:
+                self.__image = pygame.Surface((40,80))
+                self.__image.fill('chartreuse')
+            self.__rect = self.__image.get_rect(midbottom = posicao_antiga)
