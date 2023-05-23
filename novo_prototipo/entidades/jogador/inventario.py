bcaed678c47ab0bf36d60ebeb8a5c20d4d1cd465
Vitor Calegari <vitorprca@gmail.com>
2023-05-23 00:12:14 -0300

    Adiciona a possíbilidade de remover apenas um item e não o pacote todo

--- novo_prototipo/entidades/jogador/inventario.py
+++ novo_prototipo/entidades/jogador/inventario.py
@@ -1,4 +1,5 @@
 from itens.Item import Item
+from itens.ItemQuantizavel import ItemQuantizavel
 
 
 class Inventario:
@@ -16,6 +17,12 @@ class Inventario:
                 self.__itens.append(item)
                 
     def remover_item(self, item):
-        if isinstance(item, Item):
-            if item in self.__itens:
-                self.__itens.remove(item)
+        if item in self.__itens:  
+            if isinstance(item, ItemQuantizavel):
+                item.reduz_quantidade(1)
+                if item.quantidade == 0:
+                    self.__itens.remove(item)
+                return
+            if isinstance(item, Item):             
+                    self.__itens.remove(item)
+                    return
