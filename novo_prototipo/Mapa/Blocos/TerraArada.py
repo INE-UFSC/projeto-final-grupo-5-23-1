cb8ddd073f922bc38d2ca847ef13b5b37ffefc61
Vitor Calegari <vitorprca@gmail.com>
2023-05-22 23:38:11 -0300

    Adiciona interação para o sistema de plantações

--- novo_prototipo/Mapa/Blocos/TerraArada.py
+++ novo_prototipo/Mapa/Blocos/TerraArada.py
@@ -1,13 +1,22 @@
 import pygame
 from Mapa.Blocos.BlocoComInteracao import BlocoComInteracao
+from itens.sementes.Semente import Semente
 
 class TerraArada(BlocoComInteracao):
 
     def __init__(self, x, y, largura, altura, observador):
         super().__init__(x, y, largura, altura, observador)
+        self.__planta = None
+
+    def adiciona_planta(self, planta):
+        self.__planta = planta
+
+    def notifica_plantar(self, item):
+        self.observador.plantar(self.posicao_matriz[0], self.posicao_matriz[1], item)
 
     def interagir(self, item):
-        pass
+        if isinstance(item, Semente):
+            self.notifica_plantar(item)
 
     def desenhar(self, tela):
         pygame.draw.rect(tela, (155,118,83), (self.x, self.y, self.largura, self.altura))
