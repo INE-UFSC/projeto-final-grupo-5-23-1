d98a2934a4e8fab112ff155e8a651801482ceebf
Vitor Calegari <vitorprca@gmail.com>
2023-05-31 21:58:59 -0300

    Adiciona ao estado de jogo atributos para lidar com a lógica de menus

--- novo_prototipo/estado/EstadoJogo.py
+++ novo_prototipo/estado/EstadoJogo.py
@@ -1,4 +1,6 @@
 from pygame.math import Vector2
+
+from modos.ClassesAbstratas import ModoGenerico
 '''
 Esta classe representa o estado de jogo de um certo modo de jogo
 '''
@@ -6,8 +8,27 @@ class EstadoJogo():
     def __init__(self):
         self.epoch = 0
         self.__tamanho_mapa = Vector2(64,64)
+        self.__menu_ingame_ativo = False
+        self.__menu_ingame = None
         self.__observadores = [ ]
+
+    @property
+    def menu_ingame(self):
+        return self.__menu_ingame
     
+    @menu_ingame.setter
+    def menu_ingame(self, menu_ingame: ModoGenerico):
+        self.__menu_ingame = menu_ingame
+        return
+
+    @property
+    def menu_ingame_ativo(self):
+        return self.__menu_ingame_ativo
+    
+    @menu_ingame_ativo.setter
+    def menu_ingame_ativo(self, menu_ingame_ativo: bool):
+        self.__menu_ingame_ativo = menu_ingame_ativo
+
     @property
     def largura_mundo(self):
         """
