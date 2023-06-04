e28e23634f8794a4bce6d6487a8ced3d163df81b
Vitor Calegari <vitorprca@gmail.com>
2023-06-04 19:36:33 -0300

    Adiciona atributos necessários para o vendedor, mas ainda é necessário modifica-lo

--- novo_prototipo/entidades/vendedor.py
+++ novo_prototipo/entidades/vendedor.py
@@ -2,6 +2,8 @@ import pygame
 from Mapa.Blocos.BlocoComInteracao import BlocoComInteracao
 from menus.MenuVendedor import MenuVendedor
 from entidades.jogador.inventario import Inventario
+from itens.sementes.Semente1 import Semente1
+from itens.ferramentas.Enxada import Enxada
 
 # Vendedor temporariamente um bloco para MVP
 
@@ -11,15 +13,29 @@ class Vendedor(BlocoComInteracao):
         self.__image = pygame.Surface((self.largura,self.altura))
         self.__image.fill((120,120,120))
         self.__rect = self.__image.get_rect(topleft= (self.x,self.y))
+        
+        #Atributos personalidade:
+        self.__nome = "Flofler, a vendedora"
         self.__inventario = Inventario(10)
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo', quantidade = 10))
+        self.__inventario.adicionar_item(Enxada('Enxada'))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo2', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo3', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo4', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo5', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo6', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo7', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo8', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo9', quantidade = 10))
+        self.__inventario.adicionar_item(Semente1(nome = 'Trigo10', quantidade = 10))
         self.__imagem = pygame.image.load('novo_prototipo/assets/ui/vendedor_TESTE.png')
         self.__imagem_dialogo = pygame.image.load('novo_prototipo/assets/ui/dialogo_TESTE.png')
 
     def desenhar(self, tela):
         tela.blit(self.__image, self.__rect)
 
-    def interagir(self, item):
-        self.observador.notifica_ativa_menu(MenuVendedor(vendedor=self, caminho_fundo='novo_prototipo/assets/ui/fundo_menu_TESTE.png'))
+    def interagir(self, jogador):
+        self.observador.notifica_ativa_menu(MenuVendedor(vendedor=self, caminho_fundo='novo_prototipo/assets/ui/fundo_menu_TESTE.png', jogador=jogador))
 
     @property
     def rect(self):
@@ -32,6 +48,10 @@ class Vendedor(BlocoComInteracao):
     @property
     def imagem(self):
         return self.__imagem
+    
+    @property
+    def nome(self):
+        return self.__nome
     
     @property
     def inventario(self):
