eedf57ef02ac239bed1fab4f8420899becbfb5e7
Vitor Calegari <vitorprca@gmail.com>
2023-06-04 19:44:02 -0300

    Modifica imagem da semente

--- novo_prototipo/itens/sementes/Semente1.py
+++ novo_prototipo/itens/sementes/Semente1.py
@@ -2,7 +2,7 @@ from .Semente import Semente
 
 class Semente1(Semente):
 
-    def __init__(self, nome, preco = 5, caminho_imagem = 'novo_prototipo/assets/ui/sprite_semente_TESTE.png', quantidade = 0):
+    def __init__(self, nome, preco = 5, caminho_imagem = 'novo_prototipo/assets/ui/sprite_semente_TESTE.png', quantidade = 1):
         super().__init__(nome, preco, caminho_imagem, quantidade)
         self.__planta_a_ser_gerada = 'Planta 1'
 
