d1ee55c29c6f154c1158770edd9f11b91aa647ed
Vitor Calegari <vitorprca@gmail.com>
2023-06-04 19:43:48 -0300

    Modifica imagem da enxada

--- novo_prototipo/itens/ferramentas/Enxada.py
+++ novo_prototipo/itens/ferramentas/Enxada.py
@@ -2,5 +2,5 @@ from .Ferramenta import Ferramenta
 
 class Enxada(Ferramenta):
 
-    def __init__(self, nome, preco = 10, caminho_imagem = 'novo_prototipo/assets/ui/sprite_enxada_minecraft_TESTE.png'):
+    def __init__(self, nome, preco = 10, caminho_imagem = 'novo_prototipo/assets/ui/enxada_TESTE.png'):
         super().__init__(nome, preco, caminho_imagem)
