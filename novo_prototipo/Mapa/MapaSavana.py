468a705716abc0a9beec24a5ab541317564e5b5f
Gabriel Cervi <gabrielrcervi@gmail.com>
2023-06-06 17:07:34 -0300

    Criação de uma Classe para cada Mapa

---
+++ novo_prototipo/Mapa/MapaSavana.py
@@ -0,0 +1,54 @@
+import pygame
+from entidades.jogador.jogador import Jogador
+from pytmx.util_pygame import load_pygame
+from Mapa.Blocos.Grama import BlocoDeGrama
+from Mapa.Blocos.agua import Agua
+from Mapa.Blocos.Parede import Parede
+from Mapa.Blocos.Caminho import Caminho
+from Mapa.interfaces.IMapa import IMapa
+
+class MapaSavana(IMapa):
+    def __init__(self, observador):
+        super().__init__(observador)
+        self.__id = 'savana'
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    def construir_blocos(self):
+        tmx_data = load_pygame('novo_prototipo/Mapa/Mapas/savana.tmx')
+        
+        for layer in tmx_data.layers:
+            if layer.name  == 'Fundo':
+                for x, y, surf in layer.tiles():
+                    pos = (x*64, y*64)
+                    bloco = Agua(pos= pos, surf= surf, groups= self.grupoBlocos, observador=self)
+                    self.blocos[y][x] = bloco
+
+            if layer.name == 'Parede':
+                for x, y, surf in layer.tiles():
+                    pos = (x*64, y*64)
+                    bloco = Parede(pos= pos, surf= surf, groups= self.grupoBlocos, observador= self)
+                    self.blocos[y][x] = bloco
+
+            if layer.name == 'Grama':
+                for x, y, surf in layer.tiles():
+                    pos = (x*64, y*64)
+                    bloco = BlocoDeGrama(pos= pos, surf= surf, groups= self.grupoBlocos, observador= self)
+                    self.blocos[y][x] = bloco
+
+            if layer.name == 'Caminho':
+                for x, y, surf in layer.tiles():
+                    pos = (x*64, y*64)
+                    bloco = Caminho(pos= pos, surf= surf, groups= self.grupoBlocos, observador= self)
+                    self.blocos[y][x] = bloco
+
+            if layer.name == 'Interacao':
+                for obj in layer:
+                    if obj.name == 'Spawn':
+                        self.__playerSpawnX = obj.x
+                        self.__playerSpawnY = obj.y
+    
+    def adiciona_entidades(self):
+        self.entidades.append(Jogador((self.__playerSpawnX, self.__playerSpawnY), self.grupoJogador))
