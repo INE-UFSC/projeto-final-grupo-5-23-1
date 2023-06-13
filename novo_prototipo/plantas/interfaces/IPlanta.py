d196cca9e11d4ff63a0378847390b808fdce061b
Vitor Calegari <vitorprca@gmail.com>
2023-06-12 23:24:59 -0300

    Modifica IPlanta para notificar sua exclusão

--- novo_prototipo/plantas/interfaces/IPlanta.py
+++ novo_prototipo/plantas/interfaces/IPlanta.py
@@ -8,6 +8,8 @@ class IPlanta(pygame.sprite.Sprite, ABC):
         super().__init__(grupo)
         self.__nome = nome
         self.__pos = pos
+        self.__observadores = []
+        self.__em_crescimento = True
 
     @property
     def nome(self):
@@ -17,11 +19,35 @@ class IPlanta(pygame.sprite.Sprite, ABC):
     def pos(self):
         return self.__pos
     
+    @property
+    def em_crescimento(self):
+        return self.__em_crescimento
+    
+    @em_crescimento.setter
+    def em_crescimento(self, em_crescimento: bool):
+        if isinstance(em_crescimento, bool):
+            self.__em_crescimento = em_crescimento
+    
     @pos.setter
     def pos(self, pos):
         self.__pos = pos
         return
+    
+    @property
+    def observadores(self):
+        return self.__observadores
+    
+    def adiciona_observador(self, observador):
+        self.observadores.append(observador)
+
+    def notifica_exclui_planta(self):
+        for observador in self.__observadores:
+            observador.excluir_planta()
 
     @abstractmethod
     def update(self):
+        pass
+
+    @abstractmethod
+    def interagir(self, jogador):
         pass
