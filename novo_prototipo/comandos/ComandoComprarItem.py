d1f4f6617ee6346753187f2ed0bc715f921548c1
Vitor Calegari <vitorprca@gmail.com>
2023-06-17 16:58:05 -0300

    Modifica o comando de comprar itens para o novo sistema de inventario

--- novo_prototipo/comandos/ComandoComprarItem.py
+++ novo_prototipo/comandos/ComandoComprarItem.py
@@ -1,4 +1,6 @@
+from itens.Item import Item
 from comandos.Comando import Comando
+from itens.ItemQuantizavel import ItemQuantizavel
 
 
 class ComandoComprarItem(Comando):
@@ -8,6 +10,11 @@ class ComandoComprarItem(Comando):
         self.__jogador = jogador
     
     def run(self):
-        if self.__jogador.moedas - self.__item.preco >= 0:
-            self.__jogador.remove_moedas(self.__item.preco)
-            self.__jogador.inventario.adicionar_item(self.__item)
+        lista_nomes_itens = []
+        for item in self.__jogador.inventario.itens:
+            if isinstance(item, Item):
+                lista_nomes_itens.append(item.nome)
+        if (self.__jogador.inventario.capacidade_atual < self.__jogador.inventario.capacidade_maxima) or (isinstance(self.__item, ItemQuantizavel) and self.__item.nome in  lista_nomes_itens):
+            if self.__jogador.moedas - self.__item.preco >= 0:
+                self.__jogador.remove_moedas(self.__item.preco)
+                self.__jogador.inventario.adicionar_item(self.__item)
