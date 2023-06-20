8479e77bbdc72b88defe83ad897067633a49ff8c
Vitor Calegari <vitorprca@gmail.com>
2023-06-20 18:47:02 -0300

    Adiciona comportamento do trigo a terra estar regada

--- novo_prototipo/plantas/Trigo.py
+++ novo_prototipo/plantas/Trigo.py
@@ -13,10 +13,10 @@ class Trigo(IPlanta):
         self.__image.fill('saddlebrown')
         self.__rect = self.__image.get_rect(midbottom=pos)
         #--------------------------------------------------------
-        self.__taxa_de_crescimento = 10
-        self.__progresso_crescimento = 0
+        self.taxa_de_crescimento = 10
+        self.taxa_de_crescimento_base = 10
+        self.progresso_crescimento = 0
         
-
     @property
     def image(self):
         return self.__image
@@ -25,21 +25,32 @@ class Trigo(IPlanta):
     def rect(self):
         return self.__rect
     
+    
+    def atualiza_taxa_de_crescimento(self):
+        if self.observadores[0].regada:
+            self.taxa_de_crescimento = 50
+        else:
+            self.taxa_de_crescimento = self.taxa_de_crescimento_base
+    
+    def atualiza_sprite(self):
+        posicao_antiga = self.__rect.midbottom
+        if self.progresso_crescimento < 1000:
+            self.__image = pygame.Surface((5,10))
+            self.__image.fill('saddlebrown')
+        elif self.progresso_crescimento < 2000: 
+            self.__image = pygame.Surface((10,40))
+            self.__image.fill('chartreuse')
+        elif self.progresso_crescimento >= 4000:
+            self.__image = pygame.Surface((20,80))
+            self.__image.fill('yellow')
+            self.em_crescimento = False
+        self.__rect = self.__image.get_rect(midbottom = posicao_antiga)
+    
     def update(self):
+        self.atualiza_taxa_de_crescimento()
         if self.em_crescimento:
-            self.__progresso_crescimento +=  self.__taxa_de_crescimento
-            posicao_antiga = self.__rect.midbottom
-            if self.__progresso_crescimento < 1000:
-                self.__image = pygame.Surface((5,10))
-                self.__image.fill('saddlebrown')
-            elif self.__progresso_crescimento < 2000: 
-                self.__image = pygame.Surface((10,40))
-                self.__image.fill('chartreuse')
-            elif self.__progresso_crescimento < 4000:
-                self.__image = pygame.Surface((20,80))
-                self.__image.fill('yellow')
-                self.em_crescimento = False
-            self.__rect = self.__image.get_rect(midbottom = posicao_antiga)
+            self.progresso_crescimento +=  self.taxa_de_crescimento
+            self.atualiza_sprite()
 
     def interagir(self, jogador):
         if not self.em_crescimento:
