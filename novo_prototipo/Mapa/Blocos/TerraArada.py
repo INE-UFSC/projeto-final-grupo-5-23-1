4ecd199cac77b5b649450275eb8ee56f270b70ac
Vitor Calegari <vitorprca@gmail.com>
2023-06-20 18:45:03 -0300

    Adiciona mecânica de regar a terra arada

--- novo_prototipo/Mapa/Blocos/TerraArada.py
+++ novo_prototipo/Mapa/Blocos/TerraArada.py
@@ -1,6 +1,8 @@
 import pygame
 from Mapa.Blocos.interfaces.IBlocoComInteracao import IBlocoComInteracao
 from itens.sementes.ISemente import ISemente
+from itens.ferramentas.Regador import Regador
+from Mapa.Sobreposicoes.SobreposicaoAgua import SobreposicaoAgua
 from plantas.interfaces.IPlanta import IPlanta
 from settings import *
 
@@ -10,8 +12,13 @@ class TerraArada(IBlocoComInteracao):
         self.__image = pygame.image.load('novo_prototipo/Mapa/Mapas/Tilesets/tileset.png')
         self.__rect = self.__image.get_rect(topleft= pos)
         self.__textura = self.definir_textura()
-        self.__planta = None
         self.__z = LAYERS['solo']
+
+        self.__planta = None
+        self.__regada = False
+        self.__taxa_decaimento_regada = 5
+        self.__tempo_restante_regada = 0
+        self.__sobreposicao_agua = None
 
     def adiciona_planta(self, planta: IPlanta):
         if isinstance(planta, IPlanta):
@@ -27,6 +34,12 @@ class TerraArada(IBlocoComInteracao):
             self.observador.plantar(self.posicao_matriz[0], self.posicao_matriz[1], semente)
 
     def interagir(self, jogador):
+        if isinstance(jogador.item_atual, Regador):
+            self.regada = True
+            self.__tempo_restante_regada = 5000
+            self.__sobreposicao_agua = SobreposicaoAgua(posicao=self.rect.topleft)
+            self.notifica_adiciona_entidade(self.__sobreposicao_agua)
+            return
         if self.__planta == None:
             if isinstance(jogador.item_atual, ISemente):
                 self.notifica_plantar(jogador.item_atual)
@@ -47,7 +60,16 @@ class TerraArada(IBlocoComInteracao):
 
     def draw(self, tela):
         tela.blit(self.__image, self.__rect, self.__textura)
-    
+
+    def update(self):
+        if self.__regada:
+            self.__tempo_restante_regada -= self.__taxa_decaimento_regada
+            if self.__tempo_restante_regada <= 0:
+                self.__tempo_restante_regada = 0
+                self.__regada = False
+                self.notifica_exclui_entidade(self.__sobreposicao_agua)
+                self.__sobreposicao_agua = None
+
     def desenhar(self, tela):
         pass
 
@@ -66,3 +88,11 @@ class TerraArada(IBlocoComInteracao):
     @property
     def textura(self):
         return self.__textura
+
+    @property
+    def regada(self):
+        return self.__regada
+    
+    @regada.setter
+    def regada(self, regada: bool):
+        self.__regada = regada
