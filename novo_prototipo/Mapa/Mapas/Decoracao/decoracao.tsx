ee64955b145b86ca6657df9b5dbcea6292cc1674
Gabriel Cervi <gabrielrcervi@gmail.com>
2023-07-02 23:23:15 -0300

    Edição na planicie

--- novo_prototipo/Mapa/Mapas/Decoracao/decoracao.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="decoracao" tilewidth="160" tileheight="160" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="160" height="160" source="rock0.png"/>
- </tile>
-</tileset>
